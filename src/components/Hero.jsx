edfa74cf7db02c660bfc6bf9744f5357c0eaa679
cjwalt <oyedelee22@gmail.com>
2024-07-06 07:51:56 -0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,15 @@ const Hero = () => {
   return (
         <>
         <div className='relative'>
-        <main className='w-[90%] max-w-[1280px] mx-auto h-[87vh] md:h-[70vh] lg:h-[80vh] md:static md:flex md:flex-row md:flex-wrap sm:h-[25rem] sm:flex'>
+            <main className='w-[90%] border-2 max-w-[1280px] mx-auto h-[87vh] md:h-[70vh] lg:h-[80vh] md:static md:flex md:flex-row md:flex-wrap sm:h-[25rem] sm:flex'>
                 <div className='py-[3rem] self-center md:w-[50%] lg:w-[55%]'>
+                <h2 className='font-[mada] text-[1.6rem] md:text-[2rem] lg:text-[3rem] font-bold text-[#091751]'>
+                        <span className='text-[3rem]'>D</span>iscover, <span className='text-[3rem]'>D</span>ream, <span className='text-[3rem]'>D</span>rive!
+                        </h2>
                     <p className='text-[1.1rem] md:text-[1.2rem] lg:text-[1.8rem] font-semibold text-[#091751] font-[mada]'>Explore your lifestyle with us:<br/>
                         REAL ESTATE, Short Stays, AUTO Deals.
                     </p>
-                    <h2 className='font-[mada] text-[1.6rem] md:text-[2rem] lg:text-[3rem] font-bold text-[#091751]'>
-                        Discover, Dream, Drive!
-                        </h2>
+                    
                     <Link to='/popular' className='bg-[#091751] rounded-[20px] w-[90px] h-[30px] lg:h-[30px] text-[1rem] lg:w-[90px] hover:border-2 hover:bg-transparent hover:text-[#091751] hover:border-[#091751] flex items-center mt-[1rem] justify-center text-[13px] font-[inter] font-medium text-white '>Explore</Link>
 
                 </div>
@@ -23,7 +24,7 @@ const Hero = () => {
                     </div>
 
                     <div className='absolute bottom-[.6rem] right-0 slide__car'>
-                        <img src="../img/img-car.png" className='' alt="display auto hero" />
+                        <img src="../img/img-car.png" className='md:w-[30rem]' alt="display auto hero" />
                     </div>
                 </div>
                

6ca4c1831c9f9773cd0c123111a453cd5791eb99
cjwalt <oyedelee22@gmail.com>
2024-07-10 12:22:26 -0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,14 @@ const Hero = () => {
         <>
         <div className='relative'>
             <main className='w-[90%] max-w-[1280px] mx-auto h-[87vh] md:h-[70vh] lg:h-[80vh] md:static md:flex md:flex-row md:flex-wrap sm:h-[25rem] sm:flex'>
-                <div className='py-[3rem] self-center md:w-[50%] lg:w-[55%]'>
-                <h2 className='font-[mada] text-[1.6rem] md:text-[2rem] lg:text-[3rem] font-bold text-[#091751]'>
+                <div className='py-[3rem] self-center w-[20rem] md:w-[25rem] md:max-w-[28rem] lg:w-[30rem] lg:w-[55%]'>
+                <h2 className='mb-[.5rem] leading-none font-[mada] text-[1.6rem] md:text-[1.8rem] lg:text-[2rem] font-bold text-[#091751]'>
                         <span className='text-[3rem] md:text-[5rem]'>D</span>iscover, <span className='text-[3rem] md:text-[5rem]'>D</span>ream, <span className='text-[3rem] md:text-[5rem]'>D</span>rive!
                         </h2>
-                    <p className='text-[1.1rem] md:text-[1.2rem] lg:text-[1.8rem] font-semibold text-[#091751] font-[mada]'>Explore your lifestyle with us:<br/>
-                        Real estate, short stays, auto deals.
+                    <p className='text-[.8rem] leading-[1rem] md:text-[1.2rem] md:leading-[1.2rem] lg:leading-[1.8rem] lg:text-[1.4rem] font-regular text-[#091751] font-[mada]'>Explore your lifestyle with us: Real estate, short stays, auto deals, luxury properties, vacation rentals, and exclusive vehicle offers.
                     </p>
                     
-                    <Link to='/popular' className='bg-[#091751] rounded-[20px] w-[90px] h-[30px] lg:h-[30px] text-[1rem] lg:w-[90px] hover:border-2 hover:bg-transparent hover:text-[#091751] hover:border-[#091751] flex items-center mt-[1rem] justify-center text-[13px] font-[inter] font-medium text-white '>Explore</Link>
+                    <Link to='/popular' className='bg-[#091751] rounded-[20px] w-[90px] md:w-[100px] lg:w-[120px] md:h-[37px] h-[30px] lg:h-[40px] md:text-[1rem] lg:text-[1.3rem] lg:w-[90px] hover:border-2 hover:bg-transparent hover:text-[#091751] hover:border-[#091751] flex items-center mt-[1rem] justify-center text-[13px] font-[inter] font-medium text-white '>Explore</Link>
 
                 </div>
                 <div className='hero__img sm:w-[50%] sm:absolute sm:right-0 sm:bottom-0 md:h-[35rem] sm:h-[30rem]'>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,9 +5,15 @@ import OurServices from '../components/OurServices'
 import Popular from '../components/Popular'
 import Testimonial from '../components/Testimonial'
 import Faq from '../components/Faq'
+import usePageSEO from '../hooks/usePageSeo'
 
 
 const Homepage = () => {
+  usePageSEO({ 
+      title:'Excelon Properties and Auto deals',
+      description:'Excelon properties where we deliver real estate services and and auto deals',
+      image:'Excelon Properties and Auto deals',
+  })
   return (
     <div>
         
